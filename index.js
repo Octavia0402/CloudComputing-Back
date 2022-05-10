c8ab6d250ce7eb787e1bbaf538616581658a39c2
Octavia Prepelita <octavia.prepelita@gmail.com>
2022-05-10 19:09:04 +0300

    First routes and a router

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require("body-parser");
+
+const messagesRouter = require('./router/messagesRouter');
 
 const app = express();
 app.use(cors());
+// for parsing application/json
+app.use(bodyParser.json());
+// for parsing application/xwww- (encoded)
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Define routes
+app.use("/messages", messagesRouter);
 
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// });
 
 app.listen(port, () => {
     console.log(`Cloud computing app listening on port ${port}!`)
